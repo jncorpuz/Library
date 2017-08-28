2d4e535a695511407d18b2f216c4cf68871ba68a
jncorpuz <jncorpuz@icloud.com>
2017-08-28 10:22:47 +0800

    Revert "Revert "Merge remote-tracking branch 'origin/Allan_Branch' into Noel_Branch""
    This reverts commit 94d727c101b6e1c89631c029c98cefa76d26e845.

--- Library/src/Forms/LibraryBooks.java
+++ Library/src/Forms/LibraryBooks.java
@@ -10,14 +10,17 @@ package Forms;
  *
  * @author jncor
  */
-public class LibraryBooks extends javax.swing.JFrame
+import javax.swing.*;
+public class LibraryBooks extends JFrame
 {
+    static JFrame backFrame;
 
     /**
      * Creates new form LibraryBooks
      */
-    public LibraryBooks()
+    public LibraryBooks(JFrame a)
     {
+        backFrame = a;
         initComponents();
     }
 
@@ -28,38 +31,23 @@ public class LibraryBooks extends javax.swing.JFrame
      */
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents()
-    {
+    private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tbBooks = new javax.swing.JTable();
         cmdBack = new javax.swing.JButton();
         filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(0, 0), new java.awt.Dimension(0, 0), new java.awt.Dimension(0, 0));
-        jPanel1 = new javax.swing.JPanel();
-        lblPicture = new javax.swing.JLabel();
-        txtPublisher = new javax.swing.JTextField();
-        txISB = new javax.swing.JTextField();
-        txtTitle = new javax.swing.JTextField();
-        txtAuthor = new javax.swing.JTextField();
-        txtGenre = new javax.swing.JTextField();
-        txtPublicationDate = new javax.swing.JTextField();
-        txtLength = new javax.swing.JTextField();
-        txtAlternativeTitle = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
-        txtCopies = new javax.swing.JTextField();
-        jLabel9 = new javax.swing.JLabel();
-        jLabel10 = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        txtSummary = new javax.swing.JTextArea();
-        jLabel11 = new javax.swing.JLabel();
-        cmdPlaceHold = new javax.swing.JButton();
-        cmdLendBook = new javax.swing.JButton();
+        txtTitle = new javax.swing.JTextField();
+        txtISBN = new javax.swing.JTextField();
+        txtAuthor = new javax.swing.JTextField();
+        txtShelf = new javax.swing.JTextField();
+        cmdSearch = new javax.swing.JButton();
+        ddGenre = new javax.swing.JComboBox<>();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenu2 = new javax.swing.JMenu();
@@ -68,103 +56,53 @@ public class LibraryBooks extends javax.swing.JFrame
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         tbBooks.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][]
-            {
+            new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
                 {null, null, null, null},
                 {null, null, null, null}
             },
-            new String []
-            {
+            new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
         jScrollPane1.setViewportView(tbBooks);
 
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 600, 360));
+        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 220, 600, 270));
 
         cmdBack.setText("Back");
-        getContentPane().add(cmdBack, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, 60));
-        getContentPane().add(filler1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 480, 1170, 10));
-
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        jPanel1.add(lblPicture, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 10, 210, 160));
-
-        txtPublisher.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                txtPublisherActionPerformed(evt);
+        cmdBack.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdBackActionPerformed(evt);
             }
         });
-        jPanel1.add(txtPublisher, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 301, 150, -1));
-        jPanel1.add(txISB, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 180, 150, -1));
-        jPanel1.add(txtTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 211, 150, -1));
-        jPanel1.add(txtAuthor, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 241, 150, -1));
-        jPanel1.add(txtGenre, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 241, 150, -1));
-        jPanel1.add(txtPublicationDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 301, 150, -1));
-        jPanel1.add(txtLength, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 271, 150, -1));
-        jPanel1.add(txtAlternativeTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 271, 150, -1));
+        getContentPane().add(cmdBack, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 10, -1, 60));
+        getContentPane().add(filler1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 490, 620, 10));
 
-        jLabel2.setText("Summary");
-        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 370, 50, 20));
+        jLabel1.setText("Shelf Location");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 80, -1, 20));
 
-        jLabel3.setText("ISBN");
-        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 180, 30, 20));
+        jLabel2.setText("ISBN");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 80, -1, 20));
 
-        jLabel4.setText("Title");
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 211, 30, 20));
+        jLabel3.setText("Title");
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 140, -1, 20));
 
-        jLabel5.setText("Publisher");
-        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 301, 50, 20));
+        jLabel4.setText("Author");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 110, -1, 20));
 
-        jLabel6.setText("Available Copies");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 211, 90, 20));
+        jLabel5.setText("Genre");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 110, -1, 20));
+        getContentPane().add(txtTitle, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 140, 100, -1));
+        getContentPane().add(txtISBN, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 80, 100, -1));
+        getContentPane().add(txtAuthor, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 110, 100, -1));
+        getContentPane().add(txtShelf, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 80, 100, -1));
 
-        jLabel7.setText("Length");
-        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 271, 40, 20));
+        cmdSearch.setText("Search");
+        getContentPane().add(cmdSearch, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 180, 80, 30));
 
-        jLabel8.setText("Alternative Title");
-        jPanel1.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 271, 90, 20));
-        jPanel1.add(txtCopies, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 211, 150, -1));
-
-        jLabel9.setText("Author");
-        jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 241, 40, 20));
-
-        jLabel10.setText("Genre");
-        jPanel1.add(jLabel10, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 241, 40, 20));
-
-        txtSummary.setColumns(20);
-        txtSummary.setRows(5);
-        jScrollPane2.setViewportView(txtSummary);
-
-        jPanel1.add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 340, 370, 80));
-
-        jLabel11.setText("Publication Date");
-        jPanel1.add(jLabel11, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 301, 90, 20));
-
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 10, 570, 430));
-
-        cmdPlaceHold.setText("Place Hold");
-        cmdPlaceHold.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                cmdPlaceHoldActionPerformed(evt);
-            }
-        });
-        getContentPane().add(cmdPlaceHold, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 440, 190, 40));
-
-        cmdLendBook.setText("Lend Book");
-        cmdLendBook.addActionListener(new java.awt.event.ActionListener()
-        {
-            public void actionPerformed(java.awt.event.ActionEvent evt)
-            {
-                cmdLendBookActionPerformed(evt);
-            }
-        });
-        getContentPane().add(cmdLendBook, new org.netbeans.lib.awtextra.AbsoluteConstraints(820, 440, 190, 40));
+        ddGenre.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        getContentPane().add(ddGenre, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 110, 100, -1));
 
         jMenu1.setText("File");
         jMenuBar1.add(jMenu1);
@@ -177,20 +115,10 @@ public class LibraryBooks extends javax.swing.JFrame
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txtPublisherActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_txtPublisherActionPerformed
-    {//GEN-HEADEREND:event_txtPublisherActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_txtPublisherActionPerformed
-
-    private void cmdLendBookActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_cmdLendBookActionPerformed
-    {//GEN-HEADEREND:event_cmdLendBookActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_cmdLendBookActionPerformed
-
-    private void cmdPlaceHoldActionPerformed(java.awt.event.ActionEvent evt)//GEN-FIRST:event_cmdPlaceHoldActionPerformed
-    {//GEN-HEADEREND:event_cmdPlaceHoldActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_cmdPlaceHoldActionPerformed
+    private void cmdBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdBackActionPerformed
+        backFrame.setVisible(true);
+        this.dispose();
+    }//GEN-LAST:event_cmdBackActionPerformed
 
     /**
      * @param args the command line arguments
@@ -232,43 +160,29 @@ public class LibraryBooks extends javax.swing.JFrame
         {
             public void run()
             {
-                new LibraryBooks().setVisible(true);
+                new LibraryBooks(backFrame).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cmdBack;
-    public javax.swing.JButton cmdLendBook;
-    public javax.swing.JButton cmdPlaceHold;
+    private javax.swing.JButton cmdSearch;
+    private javax.swing.JComboBox<String> ddGenre;
     private javax.swing.Box.Filler filler1;
-    private javax.swing.JLabel jLabel10;
-    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JLabel lblPicture;
     private javax.swing.JTable tbBooks;
-    private javax.swing.JTextField txISB;
-    private javax.swing.JTextField txtAlternativeTitle;
     private javax.swing.JTextField txtAuthor;
-    private javax.swing.JTextField txtCopies;
-    private javax.swing.JTextField txtGenre;
-    private javax.swing.JTextField txtLength;
-    private javax.swing.JTextField txtPublicationDate;
-    private javax.swing.JTextField txtPublisher;
-    private javax.swing.JTextArea txtSummary;
+    private javax.swing.JTextField txtISBN;
+    private javax.swing.JTextField txtShelf;
     private javax.swing.JTextField txtTitle;
     // End of variables declaration//GEN-END:variables
 }
--- Library/src/Forms/ReturnBook.java
+++ Library/src/Forms/ReturnBook.java
@@ -5,24 +5,22 @@
  */
 package Forms;
 
-import javax.swing.JFrame;
-
 /**
  *
- * @author jncor
+ * @author Koala
  */
-public class LendBooks extends JFrame
-{
+import javax.swing.JFrame;
+public class ReturnBook extends JFrame {
 
     /**
-     * Creates new form LendBooks
+     * Creates new form ReturnBook
      */
     static JFrame backFrame;
-    static String bookID;
-    public LendBooks(JFrame a, String bID)
-    {
+    static String bookID,userID;
+    public ReturnBook(JFrame a, String uID,String bID) {
         backFrame = a;
         bookID = bID;
+        userID = uID;
         initComponents();
     }
 
@@ -39,15 +37,15 @@ public class LendBooks extends JFrame
         jLabel1 = new javax.swing.JLabel();
         txtISBN = new javax.swing.JTextField();
         jSeparator1 = new javax.swing.JSeparator();
-        cmdSearch = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
-        filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(0, 0), new java.awt.Dimension(0, 0), new java.awt.Dimension(32767, 32767));
-        jLabel6 = new javax.swing.JLabel();
-        txtISBN1 = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        lblDueDate = new javax.swing.JLabel();
+        txtISBN1 = new javax.swing.JTextField();
+        cmdSearch = new javax.swing.JButton();
         lblDateBorrowed = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        cmdLend = new javax.swing.JButton();
+        filler1 = new javax.swing.Box.Filler(new java.awt.Dimension(0, 0), new java.awt.Dimension(0, 0), new java.awt.Dimension(32767, 32767));
+        txtReturnDate = new javax.swing.JTextField();
         cmdBack = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -59,18 +57,10 @@ public class LendBooks extends JFrame
         getContentPane().add(txtISBN, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 190, 110, -1));
         getContentPane().add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 270, 470, 20));
 
-        cmdSearch.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        cmdSearch.setText("Search");
-        getContentPane().add(cmdSearch, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 220, 110, 40));
+        jLabel2.setText("Username");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 280, 50, 20));
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        jButton1.setText("Lend");
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 360, 110, 40));
-        getContentPane().add(filler1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 400, 470, 10));
-
-        jLabel6.setText("Date Borrowed:");
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 320, 110, 20));
-
+        txtISBN1.setEnabled(false);
         txtISBN1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 txtISBN1ActionPerformed(evt);
@@ -78,13 +68,28 @@ public class LendBooks extends JFrame
         });
         getContentPane().add(txtISBN1, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 280, 110, -1));
 
-        jLabel2.setText("Username");
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 280, 50, 20));
-
-        jLabel5.setText(" Due Date");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 320, 80, 20));
-        getContentPane().add(lblDueDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 320, 80, 20));
+        cmdSearch.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        cmdSearch.setText("Search");
+        getContentPane().add(cmdSearch, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 220, 110, 40));
         getContentPane().add(lblDateBorrowed, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 320, 80, 20));
+
+        jLabel5.setText("Return Date");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 320, 80, 20));
+
+        jLabel6.setText("Date Borrowed:");
+        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 320, 110, 20));
+
+        cmdLend.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        cmdLend.setText("Return");
+        getContentPane().add(cmdLend, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 360, 110, 40));
+        getContentPane().add(filler1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 400, 470, 10));
+
+        txtReturnDate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtReturnDateActionPerformed(evt);
+            }
+        });
+        getContentPane().add(txtReturnDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 320, 100, -1));
 
         cmdBack.setText("Back");
         cmdBack.addActionListener(new java.awt.event.ActionListener() {
@@ -101,6 +106,10 @@ public class LendBooks extends JFrame
         // TODO add your handling code here:
     }//GEN-LAST:event_txtISBN1ActionPerformed
 
+    private void txtReturnDateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtReturnDateActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtReturnDateActionPerformed
+
     private void cmdBackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdBackActionPerformed
         backFrame.setVisible(true);
         this.dispose();
@@ -109,62 +118,52 @@ public class LendBooks extends JFrame
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
          */
-        try
-        {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels())
-            {
-                if ("Nimbus".equals(info.getName()))
-                {
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex)
-        {
-            java.util.logging.Logger.getLogger(LendBooks.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex)
-        {
-            java.util.logging.Logger.getLogger(LendBooks.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex)
-        {
-            java.util.logging.Logger.getLogger(LendBooks.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex)
-        {
-            java.util.logging.Logger.getLogger(LendBooks.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(ReturnBook.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(ReturnBook.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(ReturnBook.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(ReturnBook.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable()
-        {
-            public void run()
-            {
-                new LendBooks(backFrame,bookID).setVisible(true);
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ReturnBook(backFrame, userID, bookID).setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton cmdBack;
+    private javax.swing.JButton cmdLend;
     private javax.swing.JButton cmdSearch;
     private javax.swing.Box.Filler filler1;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel lblDateBorrowed;
-    private javax.swing.JLabel lblDueDate;
     private javax.swing.JLabel lblPicture;
     private javax.swing.JTextField txtISBN;
     private javax.swing.JTextField txtISBN1;
+    private javax.swing.JTextField txtReturnDate;
     // End of variables declaration//GEN-END:variables
 }
